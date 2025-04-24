38cfa811a40f499aa2cebc2b7bada8edf728a0be
Willian Gomes Silva <25170168@EDUC123.sp.senai.br>
2025-04-24 11:14:30 -0300

    add moldura

--- funcoes/moldura.js
+++ funcoes/moldura.js
@@ -1,1 +1,42 @@
 //desenha uma moldura em uma frase
+const readline = require('readline-sync');
+
+const ch = '█'
+
+function barra(qtde) {
+  for(let i=0; i<qtde+4; i++){
+    process.stdout.write(ch);
+  }
+  console.log();
+}
+
+const frase = readline.question('Digite uma frase: ');
+
+barra(frase.length);
+barra(frase.length);
+
+
+console.log(`${ch} ${frase} ${ch}`);
+
+barra(frase.length);
+barra(frase.length);
+
+
+
+
+
+
+
+//const frase = readline.question('Digite uma frase: ')
+
+//const ch = '█'
+
+//for(let i=0; i<frase.length+4; i++){
+  //  process.stdout.write(ch);
+//}
+
+//console.log(`\n${ch} ${frase} ${ch}`);
+
+//for(let i=0; i<frase.length+4; i++){
+  //  process.stdout.write(ch);
+//}
